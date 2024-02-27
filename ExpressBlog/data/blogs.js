8eb5f8838e894073b038affa59f06b7e31442208
Avishek <avishekmondal923@gmail.com>
2024-02-27 14:11:48 +0530

    Express Blog

---
+++ ExpressBlog/data/blogs.js
@@ -0,0 +1,25 @@
+
+    blogs=[
+        {
+          title:"How to get started",
+          content:"This is Python",
+          slug:"Learn-python"  
+        },
+        {
+            title:"How to get started with JavaScript",
+            content:"This is Contenet JS",
+            slug:"Learn-JS" 
+        },
+        {
+            title:"How to use HTML in your projects",
+            content:"This is showcasing your HTML Projects",
+            slug:"Learn-HTML" 
+        },
+        {
+            title:"This is how we use Express",
+            content:"Express Applications",
+            slug:"Learn-Express" 
+        },
+    ]
+
+module.exports=blogs;
---
+++ ExpressBlog/routes/blog.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');//Require the path module
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'..', 'templates', 'index.html'));//Provide the correct path to the HTML file
+});
+
+router.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname,'..', 'templates', 'blogHome.html'));//Provide the correct path to the HTML file
+});
+
+router.get('/blogpost/:slug', (req, res) => {
+    console.log(req.params.slug)
+    myBlog=blogs.filter((item) => {
+        return item.slug==req.params.slug
+    })
+    console.log(myBlog)
+    //aisa element return kare is function se jo is function ke liye true ho..
+
+    res.sendFile(path.join(__dirname, '..', 'templates', 'blogPage.html'));
+});
+
+
+module.exports = router;
